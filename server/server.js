5e9d19349885e0a62c4166739020edc655d37954
Jon Rutishauser <jrutishauser@gmail.com>
2016-03-10 19:16:13 -0500

    server works and displays all contacts from server and also a single contact by params id in address

--- server/server.js
+++ server/server.js
@@ -1,14 +1,41 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server();
-server.connection({host: 'localhost', port: 3000});
+server.connection({host: '0.0.0.0', port: 3000});
 
-server.route({
-	method:'GET',
-	path:'/',
-	handler: function(request, reply){
-		reply('Hello, world!');
+
+var knex =  require('knex')({
+	client: 'sqlite3',
+	connection:{
+			filename: "./dev.sqlite3"
 	}
 });
+
+var bookshelf = require('bookshelf')(knex);
+
+var Contact = bookshelf.Model.extend({
+	tableName: 'contacts'	
+})
+
+server.route({
+	method: 'GET',
+	path: '/api/contacts',
+	handler: function(request, reply){
+		Contact.fetchAll().then(function(contacts){
+			reply('{"contacts:" '+ JSON.stringify(contacts) + '}');	
+		});
+	}
+})
+
+
+server.route({
+	method: 'GET',
+	path: '/api/contact/{id}',
+	handler: function(request, reply){
+			new Contact({id:request.params.id}).fetch().then(function(contact){
+			reply('{"contact:" '+ JSON.stringify(contact) + '}');	
+		});
+	}
+})
 
 server.start(function() {
 	console.log('Server running at: ', server.info.uri);
